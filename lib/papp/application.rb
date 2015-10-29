c6bc2b9b578cf3f5e1e49aa1858ea95e082c08cf
Alexander Smirnov <begdory4@gmail.com>
2015-10-29 15:30:46 +0300

    added some docs to application

--- lib/papp/application.rb
+++ lib/papp/application.rb
@@ -6,11 +6,17 @@ module PApp
   # You should inherit it
   # A good idea is to save instance in a constant
   class Application
+    # builds an applictaqion from ARGV
     def self.build!(args = ARGV)
       options = Parser.new(args).extract_options!
       new(*options)
     end
 
+    # defines key alias
+    # ==== Examples
+    #   alias_key 'f', :foo
+    #   # -f 123 => { foo: 123 }
+    #   # --f=123 => { foo: 123 }
     def self.alias_key(key, option)
       Parser.alias_key(key, option)
     end
@@ -18,6 +24,7 @@ module PApp
     alias_key :v, :verbose
     attr_accessor :logger
 
+    # `initialize` will receive all params, loaded from ARGV
     def initialize(*, verbose: '1', log_file: STDERR, **)
       v = (verbose =~ /\d+/) ? verbose.to_i : verbose.to_sym
       self.logger = Logger.new(log_file, v)
